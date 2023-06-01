d7c6385ebda7e14f4aec656256deb1337fe54b03
VladimirKW98911152 <95160748+VladimirKW98911152@users.noreply.github.com>
2023-06-02 00:50:47 +0700

    Add files via upload

---
+++ news/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import PostList, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
+urlpatterns = [
+    path('', PostList.as_view(), name='new_list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='news_detail'),
+    path('create/', PostCreateView.as_view(), name='news_create'),
+    path('create/<int:pk>', PostUpdateView.as_view(), name='news_update'),
+    path('delete/<int:pk>', PostDeleteView.as_view(), name='news_delete'),
+    path('upgrade/', upgrade_me, name='upgrade'),
+    path('categories/<int:pk>', CategoryListView.as_view(), name='category_list'),
+    path('categories/<int:pk>/subscribe', subscribe, name='subscribe')
+]
